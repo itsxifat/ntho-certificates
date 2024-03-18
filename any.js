80576e90ebb5073df656fd2e33e50bb902b14f99
itsxifat <146656340+itsxifat@users.noreply.github.com>
2024-03-18 19:42:54 +0600

    Add files via upload

---
+++ any.js
@@ -0,0 +1,251 @@
+var emails = [
+    'meem14341@gmail.com',
+    'jeniatahsin43@gmail.com',
+    'fariadtahsan121@gmail.com',
+    'mohammedrafiqulislam5878@gmail.com',
+    'abmalifhasan0168@gmail.com',
+    'tahsinlabib2005@gmail.com',
+    'anunusrat7@gmail.com',
+    'tasnim5074@gmail.com',
+    'rijvy890098@gmail.com',
+    'chamdamerz@yahoo.com',
+    'lovelyakter19t@gmail.com',
+    'saimonmahmud000999@gmail.com',
+    'adinakti2.0@gmail.com',
+    'tubamoni921@gmail.com',
+    'arianaiftekhar@gmail.com',
+    'marjina.akter@bitopibd.com',
+    'alhafizmridha@gmail.com',
+    'mm8116924@gmail.com',
+    'shammi21181@gmail.com',
+    'yasminaktersorna@gmail.com',
+    'sabidahmed2009@gmail.com',
+    'ratinmollah12@gmail.com',
+    'junayedahmed260114@gmail.com',
+    'mdsaiful307770@gmail.com',
+    'himel3623@gmail.com',
+    'mddulalmia5486@gmail.com',
+    'minhazjobayer8090@gmail.com',
+    'masudyll@gmail.com',
+    'isratjahan3137@gmail.com',
+    'ajmirealahi@gmail.com',
+    'eliashossain4405@gmail.com',
+    'sabujmullah168@gmail.com',
+    'pronitashmim@gmail.com',
+    'mayeshamehzabin6768@gmail.com',
+    'diptobhowmik77@gmail.com',
+    'tahsin.nawar2000@gmail.com',
+    'asmitora08@gmail.com',
+    'fatemaarthy72@gmail.com',
+    'sanjidahossaintorny@gmail.com',
+    'islamlata08@gmail.com',
+    'nishatnoor2020@gmail.com',
+    'uchihaitachi@gmail.com',
+    'samiyaraiyan@gmail.com',
+    'srijonthebhais1@gmail.com',
+    'tasniacr7@gmail.com',
+    'sr301523@gmail.com',
+    'saadbinkabir2020@gmail.com',
+    'nr.nishatrahman@yahoo.com',
+    'tasmiahr20@gmail.com',
+    'nahmed21221@gmail.com',
+    'abdullahalmunemnehal@gmail.com',
+    'azamkhanliton@gmail.com',
+    'zaara2shakira@gmail.com',
+    'azizsaif31@gmail.com',
+    'farzanafaria3085@gmail.com',
+    'mdsaharatsamit@gmail.com',
+    'sheikhsadialsami53@gmail.com',
+    'rezwanocean@gmail.com',
+    'tamid.ahmed.sajid@gmail.com',
+    'itsxifat22@gmail.com'];
+
+
+
+
+
+var phoneNumbers = [
+    "01642403340",
+    "01402287198",
+    "01715209335",
+    "01914225775",
+    "01991047199",
+    "01987596542",
+    "01605676477",
+    "01860671184",
+    "01978423801",
+    "01712512411",
+    "01735677233",
+    "01734526745",
+    "01710037558",
+    "01854705139",
+    "01779198035",
+    "01847347330",
+    "01629405306",
+    "01311779013",
+    "01716652758",
+    "01624746535",
+    "01783098777",
+    "01719744620",
+    "01912359313",
+    "01639307770",
+    "01737249134",
+    "01961509307",
+    "01790684949",
+    "01607342680",
+    "01933305177",
+    "01999730031",
+    "01989471566",
+    "01718380075",
+    "01715135091",
+    "01717860180",
+    "01648890962",
+    "01798617938",
+    "01968581153",
+    "01320355533",
+    "01715895819",
+    "01884573838",
+    "01613587294",
+    "01815383769",
+    "01612219892",
+    "01729147915",
+    "01581079938",
+    "01989872701",
+    "01907520487",
+    "01721491594",
+    "01913560342",
+    "01732254093",
+    "01612614455",
+    "01686668304",
+    "01732636444",
+    "01716256193",
+    "01775037463",
+    "01711264643",
+    "01730247351",
+    "01910199619",
+    "01677921944",
+    "01963949880"];
+
+var downloadLinks = [
+        'https://drive.google.com/file/d/1G8bTdKCzVijcAyVJDYjdcn1Yq12JbxXB/view?usp=drive_link',
+        'https://drive.google.com/file/d/1Gmey3DgIN6lLbZnW7UwT2X1w-ilGV9a4/view?usp=drive_link',
+        'https://drive.google.com/file/d/183-6eqIkJciZJdJ0XyuJCMeHTlBrJDB1/view?usp=drive_link',
+        'https://drive.google.com/file/d/1QD3F0eUjK1L10TyptLu5oHdNCs06_yWb/view?usp=drive_link',
+        'https://drive.google.com/file/d/1suJBGjlGaZSkSyK5d3_wtH634JfGW8ky/view?usp=drive_link',
+        'https://drive.google.com/file/d/1u33x9XI4nEuzFwpNO9y4cOjEfBdPMu-O/view?usp=drive_link',
+        'https://drive.google.com/file/d/1O4Cjv6q0iijyIDRauOy_d1ImuaGKetkR/view?usp=drive_link',
+        'https://drive.google.com/file/d/16VoW66ICRWNt5WtB4dDEZzhU7g6GJgS3/view?usp=drive_link',
+        'https://drive.google.com/file/d/1dqYCbU25jPEzZ6JG8L3D1r4n6rVfYkZT/view?usp=drive_link',
+        'https://drive.google.com/file/d/11S2K1AS-_Tw2L0T5_dVMZLUblRPFi3Ad/view?usp=drive_link',
+        'https://drive.google.com/file/d/1maSVOL3lAtCTWAJctv-VwKazZ0oEthje/view?usp=drive_link',
+        'https://drive.google.com/file/d/1YS-W0Hd6p9S0CuBGVtu88rISVMl1Of6O/view?usp=drive_link',
+        'https://drive.google.com/file/d/1jssTHmZ2INpM5ImYoecofrpzvbiLSv1-/view?usp=drive_link',
+        'https://drive.google.com/file/d/1cEgIa10g6jjRf6feg1ISrKqBruxqOGBc/view?usp=drive_link',
+        'https://drive.google.com/file/d/17HR29CNbFAq0_9Z82P0d2rBPogUWFhrk/view?usp=drive_link',
+        'https://drive.google.com/file/d/1u_vTxGaabBZJpXjpLV-88SX5wtWZpyHO/view?usp=drive_link',
+        'https://drive.google.com/file/d/1gmogC67UtjdTYZNdZdjMcYWRPBCUmEd_/view?usp=drive_link',
+        'https://drive.google.com/file/d/1aWBMq8LD7ahZKXLOo8M8x_lkIIAs6sEe/view?usp=drive_link',
+        'https://drive.google.com/file/d/18Ihlh8YvvD_C5DeyXb0Anh0tkXjV2xQK/view?usp=drive_link',
+        'https://drive.google.com/file/d/17tdi2u0hm42pxlG0C6F0a7Z4Ma8rLlvt/view?usp=drive_link',
+        'https://drive.google.com/file/d/1sDMdfBFX2lc1_DPXpZHZxVNnu5Bj4pAo/view?usp=drive_link',
+        'https://drive.google.com/file/d/1Nd26vmgXLPIldGITwFp_p6_Ucppinb3I/view?usp=drive_link',
+        'https://drive.google.com/file/d/1z-3rFNdiSesLlMyGQZSui5q90lQaAqBJ/view?usp=drive_link',
+        'https://drive.google.com/file/d/1P2Q5U-a8ZqXIP0U3FwrdwzohE3FQxL-c/view?usp=drive_link',
+        'https://drive.google.com/file/d/1f9I84bvt1IBzfvtEuP-icO_JI1yz9HKo/view?usp=drive_link',
+        'https://drive.google.com/file/d/1D47vJ_-JiGiGegMAcGZPMOWKu4J23g-0/view?usp=drive_link',
+        'https://drive.google.com/file/d/135QlFAh7Es22ZL4r3x9wr15n8Hjb1B5Y/view?usp=drive_link',
+        'https://drive.google.com/file/d/1kA2hrp7piffzMefgcnQqQXHJYPDgePbr/view?usp=drive_link',
+        'https://drive.google.com/file/d/1wzxxhrxwqTRseorLNHklIgafYwOxxlWD/view?usp=drive_link',
+        'https://drive.google.com/file/d/1c0wp2E7IXCT33tc0sI-y9-3r_8-OlL07/view?usp=drive_link',
+        'https://drive.google.com/file/d/1pamKHLg2P1xBs4A4RRnk2JBUFbl_5SCn/view?usp=drive_link',
+        'https://drive.google.com/file/d/1cJyjObb8K1cXqAhMBenASOEB-7BRixCd/view?usp=drive_link',
+        'https://drive.google.com/file/d/1wKarHjcLvzA4HeZixzy59QHb2KMjzBtS/view?usp=drive_link',
+        'https://drive.google.com/file/d/1GXtre68_etOp8O3b8nwz3wy16mJatDFh/view?usp=drive_link',
+        'https://drive.google.com/file/d/100WCnueuT-tef0ogSSq19Dy19uij1n4m/view?usp=drive_link',
+        'https://drive.google.com/file/d/1X690gXDLLmtn35aBeZ-jlx_il_gLTSpd/view?usp=drive_link',
+        'https://drive.google.com/file/d/1VoJ8Z5LDk95AKSfyJYUEc9WqF17bGiyK/view?usp=drive_link',
+        'https://drive.google.com/file/d/1qEquil7jjSzPnjKrgx91Ku_QxsmVs-KL/view?usp=drive_link',
+        'https://drive.google.com/file/d/1jCEOpjvTHYTG1E1MO-ifGhaXzdqeBX_P/view?usp=drive_link',
+        'https://drive.google.com/file/d/1ABgBxH7LmYW7AGgxNzlZs8hMK5HDAsG0/view?usp=drive_link',
+        'https://drive.google.com/file/d/1PbEAWqjp-DPOKmXYY6LS4lXihiaPVkDv/view?usp=drive_link',
+        'https://drive.google.com/file/d/1rQrD6A02jmARRR9pxxe8xt_fcDgDfErU/view?usp=drive_link',
+        'https://drive.google.com/file/d/10UeqTYMvvoA4Yvcj7chMavLucfDvcoUM/view?usp=drive_link',
+        'https://drive.google.com/file/d/1pCzKhvWfoJGxlW-GDNjc36dkGd2QI98H/view?usp=drive_link',
+        'https://drive.google.com/file/d/1VBiF_QHhb5bP9uLO4JXC4dJtjYtXZcmc/view?usp=drive_link',
+        'https://drive.google.com/file/d/1ERrXaQRJVOuaGPP3gnkcHDuf6Lx9Uwuz/view?usp=drive_link',
+        'https://drive.google.com/file/d/1voHxbuXadgnYFJe0BJrVT4tO1qlLbgTS/view?usp=drive_link',
+        'https://drive.google.com/file/d/1dqYCbU25jPEzZ6JG8L3D1r4n6rVfYkZT/view?usp=drive_link',
+        'https://drive.google.com/file/d/1JZ9fSqnIKtkzdY24nvFuuHR1_vJv6n7O/view?usp=drive_link',
+        'https://drive.google.com/file/d/1OUnh2bANyxmNstZzsgEp9nnXGG_LOLe-/view?usp=drive_link',
+        'https://drive.google.com/file/d/1OPduLq2UCpivOWcz-TKtzGzGwzxgAiFy/view?usp=drive_link',
+        'https://drive.google.com/file/d/1KJF_Bh9Sp0iobOZNnEmdkCLTg9MCvvkr/view?usp=drive_link',
+        'https://drive.google.com/file/d/1wvybbWFS-lAvRZAofnUDt0S01Qj-qOpY/view?usp=drive_link',
+        'https://drive.google.com/file/d/1R_z-IzgPeXLMaxMAUt6KXhR779wF2_MQ/view?usp=drive_link',
+        'https://drive.google.com/file/d/1OpxKQ6YgYR5h3x3MjNIgSmZGtJ1lHmfp/view?usp=drive_link',
+        'https://drive.google.com/file/d/15v-5r1hHupfxs3txoOvn4yHdGk3QdH7w/view?usp=drive_link',
+        'https://drive.google.com/file/d/1YjSjMNVBJp2LQ54P1ZsvG7O7F1gw-h7D/view?usp=drive_link',
+        'https://drive.google.com/file/d/1WQ5fdcEVBZbAHHelnv-CBe_Ux-u41scD/view?usp=drive_link',
+        'https://drive.google.com/file/d/1asGQ2Bk4_Q2DFvBvRdtZ5zE-f5mkShXa/view?usp=drive_link',
+        'https://drive.google.com/file/d/14xMw9NIVBIAIZ6hylMWPi5VnY8hEhOdR/view?usp=drive_link'
+        
+      ];
+
+      // function matchDetails() {
+      //   var email = document.getElementById('emailInput').value;
+      //   var phone = document.getElementById('phoneInput').value;
+        
+      //   var emailIndex = emails.indexOf(email);
+      //   var phoneIndex = phoneNumbers.indexOf(phone);
+
+      //   var submitBtn = document.getElementById('submitBtn');
+
+        
+      //   if (emailIndex !== -1 && emailIndex === phoneIndex) {
+      //     // Create a download button with the corresponding link
+      //     var downloadButton = document.createElement('a');
+      //     downloadButton.href = downloadLinks[emailIndex]; // Use the index to get the correct link
+      //     downloadButton.innerText = 'Download';
+      //     downloadButton.className = 'downloadButton';
+          
+      //     // Append the button to the result paragraph
+      //     var resultElement = document.getElementById('result');
+      //     resultElement.innerHTML = ''; // Clear previous results
+      //     resultElement.appendChild(downloadButton);
+      //   } else {
+      //     document.getElementById('result').innerText = "No match found.";
+      //   }
+
+
+      function matchDetails() {
+        var email = document.getElementById('emailInput').value;
+        var phone = document.getElementById('phoneInput').value;
+        
+        var emailIndex = emails.indexOf(email);
+        var phoneIndex = phoneNumbers.indexOf(phone);
+        
+        var submitBtn = document.getElementById('submitBtn');
+        var resultElement = document.getElementById('result');
+        var head = document.getElementById('head');
+        
+        if (emailIndex !== -1 && emailIndex === phoneIndex) {
+          // Hide the submit button
+          // submitBtn.style.display = 'none';
+          
+          // Create a download input and append it to the body
+          // var downloadInput = document.createElement('input');
+          // downloadInput.type = 'submit';
+          // downloadInput.value = 'Download';
+          // downloadInput.class = 'submit';
+          submitBtn.value = 'Download'
+          submitBtn.onclick = function() { window.location.href = downloadLinks[emailIndex]; };
+          submitBtn.style.background ='rgba(114, 170, 8, 0.23)'
+          head.innerText ='Verify Successful !'
+          resultElement.style.color='#fff';
+          // document.body.appendChild(downloadInput);
+          resultElement.innerText = 'Now you can download your certificate'; // Clear the result text
+        } else {
+          // Display an error message and leave the submit input unchanged
+          resultElement.innerText = "The email and phone number do not match.";
+          resultElement.style.color='rgba(255, 0, 0, 0.58)';
+        }
+      }
